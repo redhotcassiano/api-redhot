2e1c269ba37886803eedb00037618feb8865db5e
Cassiano de Souza e Silva <cassiano.silva@inone.com.br>
2018-11-23 11:42:32 -0200

    Criando logica de autenticação.

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+use Illuminate\Http\Request;
+
+Route::get('/', function () {    
     $query = http_build_query([
         'client_id' => 3,
         'redirect_url' => 'http://api.test/callback',
@@ -23,6 +25,23 @@ Route::get('/', function () {
 
     dd($query);
     return view('welcome');
+});
+
+Route::get('callback', function(Request $request){
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://api.test/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => '3',
+            'client_secret' => 'pDaOml7M8DwlLIQGp6FkuvcQzvO1BtC5FX2EawdC',
+            'redirect_uri' => 'http://api.test/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+   
 });
 
 Auth::routes();
