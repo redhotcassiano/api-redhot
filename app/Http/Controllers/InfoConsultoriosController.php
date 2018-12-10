72436c66a8b3de364ca7ec16332cdd7b574d5998
Cassiano de Souza e Silva <cassiano.silva@inone.com.br>
2018-12-10 15:36:41 -0200

    Iniciando InfoConsultorios.

---
+++ app/Http/Controllers/InfoConsultoriosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InfoConsultorios;
+use Illuminate\Http\Request;
+
+class InfoConsultoriosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InfoConsultorios  $infoConsultorios
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InfoConsultorios $infoConsultorios)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InfoConsultorios  $infoConsultorios
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InfoConsultorios $infoConsultorios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InfoConsultorios  $infoConsultorios
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InfoConsultorios $infoConsultorios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InfoConsultorios  $infoConsultorios
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InfoConsultorios $infoConsultorios)
+    {
+        //
+    }
+}
